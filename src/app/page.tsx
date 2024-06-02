9d22fe76f9947a800d22071ff202db6868369d30
Toshi7878 <toshiyuki19991125@gmail.com>
2024-06-02 21:50:00 +0900

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className=" text-2xl">(* &apos; - &apos; ) &lt; じゅんびちゅう</div>
+      <div className=" text-2xl">(* &apos; ｰ &apos; ) &lt; じゅんびちゅう</div>
     </main>
   );
 }
