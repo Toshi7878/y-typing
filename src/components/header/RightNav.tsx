43b23dd0de845b04589f994c4086c30682caf893
Toshi7878 <toshiyuki19991125@gmail.com>
2024-06-15 11:55:05 +0900

    ログイン時のみ新規作成ボタンを表示

--- src/components/header/RightNav.tsx
+++ src/components/header/RightNav.tsx
@@ -1,10 +1,13 @@
+import { auth } from "@/lib/auth";
 import Login from "./login/Login";
 import NewMap from "./new-map/NewMap";
 
-export default function RightNav() {
+export default async function RightNav() {
+  const session = await auth();
+
   return (
     <div className="flex items-center md:gap-10">
-      <NewMap />
+      {session?.user?.name ? <NewMap /> : <></>}
       <Login />
     </div>
   );
