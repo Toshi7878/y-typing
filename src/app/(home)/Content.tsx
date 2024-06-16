f2d399d95588ebe55b3fb4d62f9223e8dfce3fd7
Toshi7878 <toshiyuki19991125@gmail.com>
2024-06-16 21:08:22 +0900

    update

--- src/app/(home)/Content.tsx
+++ src/app/(home)/Content.tsx
@@ -1,90 +1,16 @@
 "use client";
-import { useEffect, useState } from "react";
-import { Card, CardBody, Box, Spinner } from "@chakra-ui/react";
-import { formatDistanceToNowStrict } from "date-fns";
-import Image from "next/image";
+import { Box } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import MapList from "./MapList";
 
-interface FetchMapList {
-  id: number;
-  title: string;
-  videoId: string;
-  updatedAt: string;
-  user: {
-    id: number;
-    name: string;
-  };
-}
-
-async function fetchMapList(): Promise<FetchMapList[]> {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/map-list`, {
-    // cache: "no-cache", // キャッシュモード
-  });
-
-  if (!response.ok) {
-    throw new Error("Failed to fetch data");
-  }
-
-  return response.json();
-}
+const queryClient = new QueryClient();
 
 export default function Content() {
-  const [mapList, setMapList] = useState<FetchMapList[]>([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    fetchMapList()
-      .then((data) => {
-        setMapList(data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error(error);
-        setLoading(false);
-      });
-  }, []);
-
-  if (loading) {
-    return <Spinner />;
-  }
-
   return (
-    <Box className="grid md:grid-cols-1 lg:grid-cols-2 gap-3">
-      {mapList.map((map) => (
-        <Card
-          key={map.id}
-          className="rounded-lg transition-shadow duration-300 hover:shadow-lg hover:shadow-teal-500/50"
-          maxW="2xl"
-          size="lg"
-        >
-          <a href={`/type/${map.id}`}>
-            <CardBody className="flex items-start bg-slate-300 rounded-lg" style={{ padding: 0 }}>
-              <Image
-                alt={map.title}
-                className="cover rounded-lg"
-                style={{ aspectRatio: "16/9" }}
-                loading="lazy"
-                src={`https://i.ytimg.com/vi_webp/${map.videoId}/mqdefault.webp`}
-                width={220}
-                height={120}
-              />
-              <Box className="ml-3 mt-2 font-bold sm:w-full md:w-[500px] text-lg">
-                <div className="text-teal-700 hover:underline">
-                  <a href={`/type/${map.id}`}>{map.title}</a>
-                </div>
-                <div>
-                  <a className="text-teal-700 hover:underline" href={`/users/${map.user.id}`}>
-                    {map.user.name}
-                  </a>
-                  <small>
-                    {" "}
-                    - {formatDistanceToNowStrict(new Date(map.updatedAt), { addSuffix: true })}
-                  </small>
-                </div>
-              </Box>
-            </CardBody>
-          </a>
-        </Card>
-      ))}
-    </Box>
+    <QueryClientProvider client={queryClient}>
+      <Box className="grid md:grid-cols-1 lg:grid-cols-2 gap-3">
+        <MapList />
+      </Box>
+    </QueryClientProvider>
   );
 }
---
+++ src/app/(home)/MapList.tsx
@@ -0,0 +1,81 @@
+"use client";
+import { Card, CardBody, Box, Spinner } from "@chakra-ui/react";
+import { formatDistanceToNowStrict } from "date-fns";
+import Image from "next/image";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+interface GetMapList {
+  id: number;
+  title: string;
+  videoId: string;
+  updatedAt: string;
+  user: {
+    id: number;
+    name: string;
+  };
+}
+
+async function getMapList(): Promise<GetMapList[]> {
+  const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/map-list`, {
+    // cache: "no-cache", // キャッシュモード
+  });
+
+  if (response.status !== 200) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return response.data;
+}
+
+export default function MapList() {
+  const { data: mapList = [], isLoading } = useQuery({
+    queryKey: ["mapList"],
+    queryFn: getMapList,
+  });
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  return (
+    <>
+      {mapList.map((map) => (
+        <Card
+          key={map.id}
+          className="rounded-lg transition-shadow duration-300 hover:shadow-lg hover:shadow-teal-500/50"
+          maxW="2xl"
+          size="lg"
+        >
+          <a href={`/type/${map.id}`}>
+            <CardBody className="flex items-start bg-slate-300 rounded-lg" style={{ padding: 0 }}>
+              <Image
+                alt={map.title}
+                className="cover rounded-lg"
+                style={{ aspectRatio: "16/9" }}
+                loading="lazy"
+                src={`https://i.ytimg.com/vi_webp/${map.videoId}/mqdefault.webp`}
+                width={220}
+                height={120}
+              />
+              <Box className="ml-3 mt-2 font-bold sm:w-full md:w-[500px] text-lg">
+                <div className="text-teal-700 hover:underline">
+                  <a href={`/type/${map.id}`}>{map.title}</a>
+                </div>
+                <div>
+                  <a className="text-teal-700 hover:underline" href={`/users/${map.user.id}`}>
+                    {map.user.name}
+                  </a>
+                  <small>
+                    {" "}
+                    - {formatDistanceToNowStrict(new Date(map.updatedAt), { addSuffix: true })}
+                  </small>
+                </div>
+              </Box>
+            </CardBody>
+          </a>
+        </Card>
+      ))}
+    </>
+  );
+}
