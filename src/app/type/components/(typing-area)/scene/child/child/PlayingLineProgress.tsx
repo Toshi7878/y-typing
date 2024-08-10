4736e27c406a326f44f20b3f86f8a40cbf59b20f
Toshi7878 <toshiyuki19991125@gmail.com>
2024-08-10 19:31:09 +0900

    update

--- src/app/type/components/(typing-area)/scene/child/child/PlayingLineProgress.tsx
+++ src/app/type/components/(typing-area)/scene/child/child/PlayingLineProgress.tsx
@@ -1,13 +1,26 @@
 import { useRefs } from "@/app/type/(contexts)/refsProvider";
-import { Box } from "@chakra-ui/react";
-import React, { forwardRef, memo, useEffect } from "react";
+import { Box, Progress } from "@chakra-ui/react";
+import React, { forwardRef, memo, useEffect, useImperativeHandle, useState } from "react";
 
 interface PlayingLineProgressProps {
   id: string;
 }
-const PlayingLineProgress = forwardRef<HTMLProgressElement, PlayingLineProgressProps>(
+
+interface PlayingProgressRef {
+  setValue: (value: number) => void;
+  setMaxValue: (value: number) => void;
+}
+const PlayingLineProgress = forwardRef<PlayingProgressRef, PlayingLineProgressProps>(
   (props, ref) => {
     const { setRef } = useRefs();
+    const [progressValue, setProgressValue] = useState(0);
+    const [maxValue, setMaxValue] = useState(100);
+
+    useImperativeHandle(ref, () => ({
+      setValue: (value: number) => setProgressValue(value),
+      setMaxValue: (value: number) => setMaxValue(value),
+    }));
+
     useEffect(() => {
       if (props.id === "line_progress") {
         if (ref && "current" in ref) {
@@ -19,10 +32,14 @@ const PlayingLineProgress = forwardRef<HTMLProgressElement, PlayingLineProgressP
 
     return (
       <Box>
-        <progress
+        <Progress
           id={props.id}
-          ref={ref as React.LegacyRef<HTMLProgressElement>}
+          ref={ref as React.Ref<HTMLDivElement>}
           className="w-full"
+          value={progressValue}
+          max={maxValue}
+          colorScheme="blue"
+          size="sm"
         />
       </Box>
     );
