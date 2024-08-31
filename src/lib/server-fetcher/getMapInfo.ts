0f7ea37489897a84f8a93605cc259c1581e8d383
Toshi7878 <toshiyuki19991125@gmail.com>
2024-08-31 13:02:09 +0900

    update

--- src/lib/server-fetcher/getMapInfo.ts
+++ src/lib/server-fetcher/getMapInfo.ts
@@ -1,10 +1,9 @@
 "use server";
 
 import { GetInfoData } from "@/types/api";
-import { cache } from "react";
 import "server-only";
 
-export const getMapInfo = cache(async (id: string): Promise<GetInfoData> => {
+export const getMapInfo = async (id: string): Promise<GetInfoData> => {
   if (id === "1") {
     return { videoId: "8iuYxdXFPbc", title: "君が代" };
   }
@@ -16,4 +15,4 @@ export const getMapInfo = cache(async (id: string): Promise<GetInfoData> => {
   }
 
   return response.json();
-});
+};
