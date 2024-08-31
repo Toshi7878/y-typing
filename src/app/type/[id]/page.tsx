c037bd41c6552367405853bf730b55f2faa40ab5
Toshi7878 <toshiyuki19991125@gmail.com>
2024-08-31 12:35:06 +0900

    update

--- src/app/type/[id]/page.tsx
+++ src/app/type/[id]/page.tsx
@@ -1,7 +1,25 @@
 import Content from "./content";
 import { Metadata } from "next";
-import { getMapInfo } from "@/lib/server-fetcher/getMapInfo";
 import TypeProvider from "./TypeProvider";
+
+import { GetInfoData } from "@/types/api";
+import { cache } from "react";
+
+export const getMapInfo = cache(async (id: string): Promise<GetInfoData> => {
+  if (id === "1") {
+    return { videoId: "8iuYxdXFPbc", title: "君が代" };
+  }
+
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/map-info?id=${id}`, {
+    cache: "no-cache",
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return response.json();
+});
 
 export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
   const mapInfo = await getMapInfo(params.id);
