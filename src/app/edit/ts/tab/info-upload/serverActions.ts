f592c97024cf1dbee7e9a662a8cf44b4830ef42b
Toshi7878 <toshiyuki19991125@gmail.com>
2024-09-15 14:33:11 +0900

    update

--- src/app/edit/ts/tab/info-upload/serverActions.ts
+++ src/app/edit/ts/tab/info-upload/serverActions.ts
@@ -82,6 +82,7 @@ export async function actions(data: EditorSendData, mapId: string): Promise<Uplo
       }
     }
     revalidatePath("/api/map-list");
+    revalidateTag("mapList");
 
     return {
       id: newMapId,
