2323c103e1f3d8eb628da4a762b22542cf5155af
Toshi7878 <toshiyuki19991125@gmail.com>
2024-09-15 14:28:24 +0900

    update

--- src/app/edit/ts/tab/info-upload/serverActions.ts
+++ src/app/edit/ts/tab/info-upload/serverActions.ts
@@ -71,6 +71,7 @@ export async function actions(data: EditorSendData, mapId: string): Promise<Uplo
       if (mapCreatorId?.creatorId === userId || userRole === "admin") {
         newMapId = await updateMap(data, Number(mapId));
         revalidatePath(`/api/map-info?id=${newMapId}`);
+        revalidateTag("map-info");
       } else {
         return {
           id: null,
--- src/lib/server-fetcher/getMapInfo.ts
+++ src/lib/server-fetcher/getMapInfo.ts
@@ -4,8 +4,9 @@ import "server-only";
 import { cache } from "react";
 
 export const getMapInfo = cache(async (id: string): Promise<GetInfoData> => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/map-info?id=${id}`);
-
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/map-info?id=${id}`, {
+    next: { tags: ["map-info"] },
+  });
   if (!response.ok) {
     throw new Error(`Failed to fetch data: ${response.status} ${response.statusText}`);
   }
