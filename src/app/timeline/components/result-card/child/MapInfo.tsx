75f3d4f069d345f06da9b0ee85192b8b295a5746
Toshi7878 <toshiyuki19991125@gmail.com>
2024-10-05 16:30:43 +0900

    update

--- src/app/timeline/components/result-card/child/MapInfo.tsx
+++ src/app/timeline/components/result-card/child/MapInfo.tsx
@@ -1,28 +1,36 @@
 "use client";
 import { Box, Flex, useTheme } from "@chakra-ui/react";
 import CustomToolTip from "@/components/CustomToolTip";
-import { MapCardInfo } from "@/app/(home)/ts/type";
 import { ResultCardInfo } from "@/app/timeline/ts/type";
 import { ThemeColors } from "@/types";
+import { Link } from "@chakra-ui/next-js";
+import { useLinkClick } from "@/lib/hooks/useLinkClick";
 
 interface MapCardProps {
   map: ResultCardInfo["map"];
 }
 function MapInfo({ map }: MapCardProps) {
   const theme: ThemeColors = useTheme();
+  const handleLinkClick = useLinkClick();
+
   return (
     <Flex direction="column" gap={1}>
       <CustomToolTip tooltipLabel={`${map.title} / ${map.artistName}`} placement="top">
-        <Box
+        <Link
+          href={`/type/${map.id}`}
+          onClick={handleLinkClick}
           color={theme.colors.home.card.link}
-          fontWeight="bold"
-          overflow="hidden"
-          textOverflow="ellipsis"
-          whiteSpace="nowrap"
-          fontSize="md"
         >
-          {`${map.title} / ${map.artistName}`}
-        </Box>
+          <Box
+            fontWeight="bold"
+            overflow="hidden"
+            textOverflow="ellipsis"
+            whiteSpace="nowrap"
+            fontSize="md"
+          >
+            {`${map.title} / ${map.artistName}`}
+          </Box>
+        </Link>
       </CustomToolTip>
     </Flex>
   );
