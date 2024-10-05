ed52319bc3410c08c94dd3b8ec4029425ee9d435
Toshi7878 <toshiyuki19991125@gmail.com>
2024-10-05 17:43:29 +0900

    update

--- src/app/timeline/ts/const/consts.ts
+++ src/app/timeline/ts/const/consts.ts
@@ -1,6 +1,4 @@
-export const THUBNAIL_WIDTH = { base: 120, sm: 170, xl: 120 };
+export const THUBNAIL_WIDTH = { base: 140 };
 export const THUBNAIL_HEIGHT = {
   base: (THUBNAIL_WIDTH.base * 9) / 16,
-  sm: (THUBNAIL_WIDTH.sm * 9) / 16,
-  xl: (THUBNAIL_WIDTH.xl * 9) / 16,
 };
