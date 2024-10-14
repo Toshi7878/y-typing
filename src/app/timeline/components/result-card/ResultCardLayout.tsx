0154260122fc5e3cda5ca1bd9dda1a2760d61d9e
Toshi7878 <toshiyuki19991125@gmail.com>
2024-10-14 21:42:47 +0900

    update

--- src/app/timeline/components/result-card/ResultCardLayout.tsx
+++ src/app/timeline/components/result-card/ResultCardLayout.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const ResultCardLayout = ({ children }) => {
   return (
     <Box
-      className="w-[100vw] md:w-[90vw] lg:w-[90vw] 2xl:w-[65vw]"
+      className="w-[100%] md:w-[90vw] lg:w-[90vw] 2xl:w-[65vw]"
       display="grid"
       gridTemplateColumns={{ base: "1fr" }}
       gap={3}
