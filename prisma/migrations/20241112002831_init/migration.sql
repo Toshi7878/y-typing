afdb532bb3ca476bc87656bedb5a17ce9d03b170
Toshi7878 <toshiyuki19991125@gmail.com>
2024-11-15 03:45:02 +0900

    update

--- prisma/migrations/20241112002831_init/migration.sql
+++ prisma/migrations/20241112002831_init/migration.sql
@@ -1,3 +1,7 @@
 -- AlterTable
 ALTER TABLE "Map" ALTER COLUMN "artistName" DROP NOT NULL,
 ALTER COLUMN "musicSource" DROP NOT NULL;
+
+CREATE EXTENSION IF NOT EXISTS pgroonga;
+create index ix_maps_search on public."Map" using pgroonga ("title", "artistName", "musicSource", "tags");
+create index ix_user_name_search on public."User" using pgroonga ("name");
