604770ebd6b163104784a34308dd43720e00fdb7
Toshi7878 <toshiyuki19991125@gmail.com>
2024-11-15 20:17:13 +0900

    update

---
+++ prisma/migrations/20241115111647_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `mapData` on the `Map` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Map" DROP COLUMN "mapData";
