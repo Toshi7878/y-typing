ff09d312d342e5708ca3951a2efc65192d3fbdae
Toshi7878 <toshiyuki19991125@gmail.com>
2024-11-18 20:43:57 +0900

    update

--- src/app/api/update-typing-option/route.ts
+++ src/app/api/update-typing-option/route.ts
@@ -2,7 +2,6 @@
 
 import { PrismaClient } from "@prisma/client";
 import { auth } from "@/lib/auth";
-import { UploadResult } from "@/types";
 import { UserTypingOptions } from "@/app/type/ts/type";
 import { NextRequest } from "next/server";
 
@@ -25,7 +24,7 @@ async function updateTypingOption(userId: number, updateData: UserTypingOptions)
   return updated.id;
 }
 
-export async function POST(req: NextRequest): Promise<UploadResult> {
+export async function POST(req: NextRequest): Promise<Response> {
   const session = await auth();
 
   const updateData: UserTypingOptions = await req.json();
@@ -35,19 +34,25 @@ export async function POST(req: NextRequest): Promise<UploadResult> {
 
     const id = await updateTypingOption(userId, updateData);
 
-    return {
-      id,
-      title: "アップデート完了",
-      message: "",
-      status: 200,
-    };
+    return new Response(
+      JSON.stringify({
+        id,
+        title: "アップデート完了",
+        message: "",
+        status: 200,
+      }),
+      { status: 200 },
+    );
   } catch (error) {
-    return {
-      id: null,
-      title: "サーバー側で問題が発生しました",
-      message: "しばらく時間をおいてから再度お試しください。",
-      status: 500,
-      errorObject: error instanceof Error ? error.message : String(error), // エラーオブジェクトを文字列に変換
-    };
+    return new Response(
+      JSON.stringify({
+        id: null,
+        title: "サーバー側で問題が発生しました",
+        message: "しばらく時間をおいてから再度お試しください。",
+        status: 500,
+        errorObject: error instanceof Error ? error.message : String(error),
+      }),
+      { status: 500 },
+    );
   }
 }
