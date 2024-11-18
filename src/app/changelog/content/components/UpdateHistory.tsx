e1d23ecdd37326d7513ee63576c999d65c1bc81c
Toshi7878 <toshiyuki19991125@gmail.com>
2024-11-18 21:11:27 +0900

    update

--- src/app/changelog/content/components/UpdateHistory.tsx
+++ src/app/changelog/content/components/UpdateHistory.tsx
@@ -4,6 +4,10 @@ import React from "react";
 
 const updates = [
   {
+    date: "2024-11-18",
+    descriptions: ["タイピング設定の全体タイミング調整・次のワード表示方法を追加"],
+  },
+  {
     date: "2024-11-17",
     descriptions: ["譜面一覧にランキング登録数表示・登録済み色を追加", "いいね機能追加"],
   },
