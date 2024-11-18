0e1006d56fa5463773b7ea5ce7813da64a26a0d1
Toshi7878 <toshiyuki19991125@gmail.com>
2024-11-18 21:43:45 +0900

    update

--- src/app/type/hooks/playing-hooks/usePressSkip.ts
+++ src/app/type/hooks/playing-hooks/usePressSkip.ts
@@ -1,19 +1,25 @@
 import { RefObject } from "react";
 import { SkipGuideRef } from "../../components/typing-area/scene/playing-child/child/PlayingSkipGuide";
 import { CreateMap } from "../../ts/scene-ts/ready/createTypingWord";
-import { useMapAtom, useTypePageSpeedAtom } from "../../type-atoms/gameRenderAtoms";
+import {
+  useMapAtom,
+  useTypePageSpeedAtom,
+  useUserOptionsAtom,
+} from "../../type-atoms/gameRenderAtoms";
 import { useRefs } from "../../type-contexts/refsProvider";
 
 export const usePressSkip = () => {
   const { statusRef, gameStateRef, ytStateRef, playerRef } = useRefs();
   const map = useMapAtom() as CreateMap;
   const speedData = useTypePageSpeedAtom();
+  const userOptionsAtom = useUserOptionsAtom();
 
   return (skipGuideRef: RefObject<SkipGuideRef>) => {
     const nextLine = map!.mapData[statusRef.current!.status.count];
-    const skippedTime = gameStateRef.current!.isRetrySkip
-      ? Number(map!.mapData[map!.startLine]["time"])
-      : Number(nextLine["time"]);
+    const skippedTime =
+      (gameStateRef.current!.isRetrySkip
+        ? Number(map!.mapData[map!.startLine]["time"])
+        : Number(nextLine["time"])) - userOptionsAtom.timeOffset;
 
     const seekTime =
       nextLine["lyrics"] === "end"
