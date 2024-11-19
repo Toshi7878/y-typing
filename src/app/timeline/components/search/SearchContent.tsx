3627fb61d353e367f268fd56f644480a07d57ee4
Toshi7878 <toshiyuki19991125@gmail.com>
2024-11-19 22:46:13 +0900

    update

--- src/app/timeline/components/search/SearchContent.tsx
+++ src/app/timeline/components/search/SearchContent.tsx
@@ -2,17 +2,28 @@ import { Box, Flex } from "@chakra-ui/react";
 import React from "react";
 import SearchCard from "./child/SearchModal";
 import SearchInputs from "./child/SearchInputs";
+import { useGlobalRefs } from "@/components/globalRefContext/GlobalRefProvider";
+import VolumeRange from "@/components/custom-chakra-ui/VolumeRange";
 
 const SearchContent = () => {
+  const isIOS = typeof navigator !== "undefined" && /iPhone|iPad|iPod/i.test(navigator.userAgent);
+  const isAndroid = typeof navigator !== "undefined" && /Android/i.test(navigator.userAgent);
+  const { playerRef } = useGlobalRefs();
+
   return (
     <Flex as="section" width="100%" alignItems="center" mb={4}>
       <Box width="100%">
         <Box mb={3}>
           <SearchInputs />
         </Box>
-        <Box>
+        <Flex justifyContent="space-between">
           <SearchCard />
-        </Box>
+          {!isIOS && !isAndroid && (
+            <Flex justifyContent="flex-end">
+              <VolumeRange playerRef={playerRef} />
+            </Flex>
+          )}
+        </Flex>
       </Box>
     </Flex>
   );
