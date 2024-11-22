2bc6450534d60fcd0e43062352dc7218debd9e39
Toshi7878 <toshiyuki19991125@gmail.com>
2024-11-22 20:29:27 +0900

    update

--- src/app/changelog/content/Content.tsx
+++ src/app/changelog/content/Content.tsx
@@ -1,7 +1,15 @@
+"use client";
 import UpdateHistory from "./components/UpdateHistory";
 import ContentHeading from "./components/Heading";
+import { useEffect } from "react";
+import nProgress from "nprogress";
 
 const Content = () => {
+  useEffect(() => {
+    window.getSelection()!.removeAllRanges();
+    nProgress.done();
+  }, []);
+
   return (
     <>
       <ContentHeading />
