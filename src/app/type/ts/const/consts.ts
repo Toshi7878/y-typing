1faee798bf719e5543d062cb821be1773626f73e
Toshi7878 <toshiyuki19991125@gmail.com>
2024-11-22 12:52:37 +0900

    update

--- src/app/type/ts/const/consts.ts
+++ src/app/type/ts/const/consts.ts
@@ -1,8 +1,8 @@
 export const RANKING_COLUMN_WIDTH = {
   rank: "7%",
-  score: "12%",
+  score: "10%",
   clearRate: "10%",
-  userName: "34%",
+  userName: "36%",
   kpm: "7%",
   inputMode: "15%",
   updatedAt: "10%",
