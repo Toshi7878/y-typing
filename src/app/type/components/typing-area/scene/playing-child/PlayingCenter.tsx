0a51cd3f5c7ca1083bad7c8e8084c1918e992db1
Toshi7878 <toshiyuki19991125@gmail.com>
2024-11-30 19:40:43 +0900

    update

--- src/app/type/components/typing-area/scene/playing-child/PlayingCenter.tsx
+++ src/app/type/components/typing-area/scene/playing-child/PlayingCenter.tsx
@@ -1,6 +1,9 @@
+import { usePressSkip } from "@/app/type/hooks/playing-hooks/usePressSkip";
+import { skipAtom } from "@/app/type/type-atoms/gameRenderAtoms";
 import { useRefs } from "@/app/type/type-contexts/refsProvider";
 import "@/css/type.css";
 import { VStack } from "@chakra-ui/react";
+import { useStore } from "jotai";
 import { CARD_BODY_MIN_HEIGHT } from "../../TypingCard";
 import PlayingChangeCSS from "./child/PlayingChangeCSS";
 import PlayingLyrics from "./child/PlayingLyrics";
@@ -16,6 +19,9 @@ const PlayingCenter = ({ flex }: PlayingCenterProps) => {
 
   const playMode = gameStateRef.current!.playMode;
 
+  const pressSkip = usePressSkip();
+  const typeAtomStore = useStore();
+
   return (
     <VStack
       sx={{ cursor: playMode === "playing" ? "none" : "auto" }}
@@ -27,6 +33,14 @@ const PlayingCenter = ({ flex }: PlayingCenterProps) => {
       justifyContent="space-between"
       style={{ userSelect: "none", cursor: "none" }}
       id="typing_scene"
+      onTouchStart={() => {
+        const skip = typeAtomStore.get(skipAtom);
+        // タップ時の処理をここに記述
+
+        if (skip) {
+          pressSkip();
+        }
+      }}
     >
       <PlayingTypingWords />
       <PlayingLyrics />
