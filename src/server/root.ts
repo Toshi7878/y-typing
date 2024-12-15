d633eacf8138d4c0596666475053caadfcef5070
Toshi7878 <toshiyuki19991125@gmail.com>
2024-12-15 14:43:35 +0900

    update

--- src/server/root.ts
+++
@@ -1,7 +0,0 @@
-import { userRouter } from "./router/user-router";
-import { router } from "./trpc";
-
-export const appRouter = router({
-  user: userRouter,
-});
-export type AppRouter = typeof appRouter; // クライアント側で型推論するために型をエクスポート
